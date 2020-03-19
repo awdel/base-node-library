1a25605821947cf2375873928ca6aec18e59723f
Adam Delaney <adelaney@mailfence.com>
2020-03-19 13:03:10 +0000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = (env, argv) => {
+    return {
+        entry: './src/javascript/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/scripts.js',
+            library: 'myLibrary',
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        },
+        devtool: argv.mode === 'development' ? 'source-map' : false,
+        watch: argv.mode === 'development' ? true : false,
+        watchOptions: {
+            ignored: /node_modules/,
+            poll: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)|(bower_components)/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['@babel/preset-env']
+                            }
+                        },
+                        {
+                            loader: "eslint-loader"
+                        }
+                    ]
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: "css-loader",
+                        },
+                        {
+                            loader: "postcss-loader",
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                implementation: require("sass")
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "css/styles.css"
+            })
+        ]
+    };
+};
